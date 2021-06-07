8afaa3f53d6c3c2c84c51218969df95f338b5f36
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2021-06-07 10:59:53 +0200

    Update Keycloak.php

--- src/Provider/Keycloak.php
+++ src/Provider/Keycloak.php
@@ -174,7 +174,18 @@ class Keycloak extends AbstractProvider
      */
     protected function getDefaultScopes()
     {
-        return ['name', 'email'];
+        return ['profile', 'email'];
+    }
+
+    /**
+     * Returns the string that should be used to separate scopes when building
+     * the URL for requesting an access token.
+     *
+     * @return string Scope separator, defaults to ','
+     */
+    protected function getScopeSeparator()
+    {
+        return ' ';
     }
 
     /**
